57249f14cc0cc042971a13acc4146715e436f690
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-08-08 10:18:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+//Node server
+const io = require('socket.io')(8000,{
+    cors: {
+        origin : 'http://127.0.0.1:5500', // Allow requests from this origin
+        methods: ['GET', 'POST', 'PUT'], // Allowed methods
+        allowedHeaders: ['Content-Type'], // Allowed headers
+        credentials :true
+    }
+});
+
+const users = {};
+io.on('connection', socket =>{
+    socket.on('new-user-joined',name =>{
+        console.log("New User", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name)
+    });
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    });
+
+
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+
+})
